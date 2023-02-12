7766e67883ad8a96f834dde97d2c592c73a4b015
truland <tnfirebird1979@gmail.com>
2023-02-12 17:08:50 -0500

    Made the genscan take in the options and create the Unpacker

--- Core/include/genscan.hpp
+++ Core/include/genscan.hpp
@@ -9,15 +9,25 @@
 
 #include <sys/stat.h> //For directory manipulation
 
+#include "Unpacker.hpp"
+
 class genscan{
 	public:
 		genscan();
 		~genscan() = default;
-		void SetCfgFile(std::string);
 
+		void SetCfgFile(std::string*);
+		void SetInputFile(std::string*);
+		void SetOutputFile(std::string*);
+		void SetEvtBuild(bool);
+
+		void DoScan();
 	private:
-		std::string cfgFile;
+		std::string* cfgFile;
+		std::string* inputFile;
+		std::string* outputFile;
+		bool evtBuild;
 
-
+		Unpacker* unpacker;
 };
 #endif
--- Core/src/genscan.cpp
+++ Core/src/genscan.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
+
 #include "genscan.hpp"
+
+#include "CompassROOTUnpacker.hpp"
 
 genscan::genscan(){
 }
 
-void genscan::SetCfgFile(std::string a){
-	cfgFile = a;
+void genscan::SetCfgFile(std::string* cfg){
+	cfgFile = cfg;
+}
+
+void genscan::SetInputFile(std::string* infile){
+	inputFile = infile;
+}
+
+void genscan::SetOutputFile(std::string* outfile){
+	outputFile = outfile;
+}
+
+void genscan::SetEvtBuild(bool evtbuild){
+	evtBuild = evtbuild;
+}
+
+void genscan::DoScan(){
+	//need to determine which unpacker to use
+	//either do this based on file extension of by command line option?
+	//default to a specific file extension?
+	//probably safest to just read the file extension
+	
+	unpacker = new CompassROOTUnpacker();
+	unpacker->SetCurrFile(inputFile);
+	std::cout << "Current File : " << *(unpacker->GetCurrFile()) << std::endl;
 }
