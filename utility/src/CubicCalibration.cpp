33f47ee1776bcea253a65d9fcb4ea0a807c43659
truland <tnfirebird1979@gmail.com>
2023-06-19 23:34:46 -0400

    Removing old technique which while easier to maintain is slow af

--- utility/src/CubicCalibration.cpp
+++
@@ -1,8 +0,0 @@
-#include "CubicCalibration.hpp"
-
-CubicCalibration::CubicCalibration(std::vector<double>& p) : Calibration("Cubic",p){
-}
-
-double CubicCalibration::Calibrate(double& erg){
-	return Params.at(0) + Params.at(1)*erg + Params.at(2)*erg*erg + Params.at(3)*erg*erg*erg;
-}
