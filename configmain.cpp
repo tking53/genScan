7ac9e0e7be78b7064c7d3eeb48b5d98f46b3c972
truland <tnfirebird1979@gmail.com>
2023-11-24 01:03:50 -0600

    Added in GenConfig executable that will generate a template config file that will demonstrate everything we can use in the GenScanor program

---
+++ configmain.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/cfg/env.h>
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include "GenConfigArgParser.hpp"
+
+int main(int argc, char *argv[]) {
+	spdlog::set_level(spdlog::level::debug);
+	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> LogFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("genconfig.log",true);
+	LogFileSink->set_level(spdlog::level::info);
+
+	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> ErrorFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("genconfig.err",true);
+	ErrorFileSink->set_level(spdlog::level::err);
+
+	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> DebugFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("genconfig.dbg",true);
+	DebugFileSink->set_level(spdlog::level::debug);
+
+	std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> LogFileConsole = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	LogFileConsole->set_level(spdlog::level::info);
+
+	std::vector<spdlog::sink_ptr> sinks {DebugFileSink,LogFileSink,ErrorFileSink,LogFileConsole};
+	auto console = std::make_shared<spdlog::logger>("genconfig",sinks.begin(),sinks.end());
+	spdlog::initialize_logger(console);
+
+
+	auto cmdArgs = GenConfigArgParser::Get(argv[0]);
+	try{
+		cmdArgs->ParseArgs(argc,argv);
+	}catch( std::runtime_error const& e ){
+		console->error(e.what());
+		exit(EXIT_FAILURE);
+	}
+
+}
