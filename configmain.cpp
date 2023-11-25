b5d6a6a3efa431af9278ad1ed54829ed60ae7bf5
truland <tnfirebird1979@gmail.com>
2023-11-24 22:51:32 -0600

    Added in updates related to different logname

--- configmain.cpp
+++ configmain.cpp
@@ -10,25 +10,31 @@
 #include "GenConfigArgParser.hpp"
 
 int main(int argc, char *argv[]) {
+
+	std::string logname = "genconfig";
+	std::string logfilename = logname+".log";
+	std::string errfilename = logname+".err";
+	std::string dbgfilename = logname+".dbg";
+
 	spdlog::set_level(spdlog::level::debug);
-	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> LogFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("genconfig.log",true);
+	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> LogFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logfilename,true);
 	LogFileSink->set_level(spdlog::level::info);
 
-	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> ErrorFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("genconfig.err",true);
+	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> ErrorFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(errfilename,true);
 	ErrorFileSink->set_level(spdlog::level::err);
 
-	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> DebugFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("genconfig.dbg",true);
+	std::shared_ptr<spdlog::sinks::basic_file_sink_mt> DebugFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(dbgfilename,true);
 	DebugFileSink->set_level(spdlog::level::debug);
 
 	std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> LogFileConsole = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 	LogFileConsole->set_level(spdlog::level::info);
 
 	std::vector<spdlog::sink_ptr> sinks {DebugFileSink,LogFileSink,ErrorFileSink,LogFileConsole};
-	auto console = std::make_shared<spdlog::logger>("genconfig",sinks.begin(),sinks.end());
+	auto console = std::make_shared<spdlog::logger>(logname,sinks.begin(),sinks.end());
 	spdlog::initialize_logger(console);
 
 
-	auto cmdArgs = GenConfigArgParser::Get(argv[0]);
+	auto cmdArgs = GenConfigArgParser::Get(argv[0],logname);
 	try{
 		cmdArgs->ParseArgs(argc,argv);
 	}catch( std::runtime_error const& e ){
