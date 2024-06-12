d63a6a3bf4319f94d876e52710e4901dcfa7a539
truland <tnfirebird1979@gmail.com>
2024-06-12 13:57:11 +0900

    Initial PSPMTProcessor for 108Xe ribf168 experiment

---
+++ processors/include/PSPMTProcessor.hpp
@@ -0,0 +1,67 @@
+#ifndef __PSPMT_PROCESSOR_HPP__
+#define __PSPMT_PROCESSOR_HPP__
+
+#include <string>
+#include <vector>
+#include <utility>
+
+#include "EventSummary.hpp"
+#include "HistogramManager.hpp"
+#include "Processor.hpp"
+
+class PSPMTProcessor : public Processor{
+	public:
+		PSPMTProcessor(const std::string&);
+		[[maybe_unused]] bool PreProcess([[maybe_unused]] EventSummary&,[[maybe_unused]] PLOTS::PlotRegistry*,[[maybe_unused]] CUTS::CutRegistry*) final;
+		[[maybe_unused]] bool Process(EventSummary&,[[maybe_unused]] PLOTS::PlotRegistry*,[[maybe_unused]] CUTS::CutRegistry*) final;
+		[[maybe_unused]] bool PostProcess([[maybe_unused]] EventSummary&,[[maybe_unused]] PLOTS::PlotRegistry*,[[maybe_unused]] CUTS::CutRegistry*) final;
+
+		virtual void Finalize() final;
+
+		void Init(const YAML::Node&);
+		void Init(const Json::Value&);
+		void Init(const pugi::xml_node&);
+
+		void DeclarePlots(PLOTS::PlotRegistry*) const;
+		virtual void RegisterTree([[maybe_unused]] std::unordered_map<std::string,TTree*>&) final;
+		virtual void CleanupTree() final;
+
+		struct Image{
+			double dynode;
+			double xa;
+			double xb;
+			double ya;
+			double yb;
+			double anodesum;
+			int numanodes;
+			std::pair<double,double> position;
+		};
+
+		struct EventInfo{
+			Image hg;
+			Image lg;
+			double ampdynode;
+			bool Pileup;
+			bool Saturate;
+			bool RealEvt;
+		};
+
+	private:
+		void Reset();
+		void CalculatePosition(Image&,double,double,double,bool);
+		
+		EventInfo CurrEvt;
+		EventInfo PrevEvt;
+		EventInfo NewEvt;
+
+		int DynodeHighHits;
+		int DynodeLowHits;
+		int AmpDynodeHits;
+		std::vector<int> AnodeLowHits;
+		std::vector<int> AnodeHighHits;
+
+		std::string highgaintag;
+		std::string lowgaintag;
+};
+
+#endif
---
+++ processors/src/PSPMTProcessor.cpp
@@ -0,0 +1,228 @@
+#include "PSPMTProcessor.hpp"
+#include "CutManager.hpp"
+#include "EventSummary.hpp"
+#include "HistogramManager.hpp"
+#include <TTree.h>
+#include <stdexcept>
+
+PSPMTProcessor::PSPMTProcessor(const std::string& log) : Processor(log,"PSPMTProcessor",{"pspmt"}){
+	this->NewEvt = {
+		.hg = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, { -10.0, -10.0 }},
+		.lg = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, { -10.0, -10.0 }},
+		.ampdynode = 0.0,
+		.Pileup = false,
+		.Saturate = false,
+		.RealEvt = false
+	};
+	this->CurrEvt = this->NewEvt;
+	this->PrevEvt = this->CurrEvt;
+	this->highgaintag = "highgain";
+	this->lowgaintag = "lowgain";
+}
+
+[[maybe_unused]] bool PSPMTProcessor::PreProcess(EventSummary& summary,[[maybe_unused]] PLOTS::PlotRegistry* hismanager,[[maybe_unused]] CUTS::CutRegistry* cutmanager){
+	Processor::PreProcess();
+
+	this->Reset();
+
+	summary.GetDetectorSummary(this->AllDefaultRegex["pspmt"],this->SummaryData);
+	for( const auto& evt : this->SummaryData ){
+		auto subtype = evt->GetSubType();
+		auto group = evt->GetGroup();
+		bool islowgain = evt->HasTag(this->lowgaintag);
+		bool ishighgain = evt->HasTag(this->highgaintag);
+
+		if( (ishighgain and islowgain) or (not ishighgain and not islowgain) ){
+			throw std::runtime_error("evt in PSPMTProcessor has malformed xml, has both lowgain and highgain tag or neither");
+		}
+
+		if( evt->GetSaturation() ){
+			this->CurrEvt.Saturate = true;
+		}
+		if( evt->GetPileup() ){
+			this->CurrEvt.Pileup = true;
+		}
+
+		if( subtype.compare("anode") == 0 ){
+			if( group.compare("xa") == 0 ){
+				if( islowgain ){
+					if( not this->AnodeLowHits[0] ){
+						++this->AnodeLowHits[0];
+						this->CurrEvt.lg.xa += evt->GetEnergy();
+						++this->CurrEvt.lg.numanodes;
+					}else{
+						++this->AnodeLowHits[0];
+					}
+				}else{
+					if( not this->AnodeHighHits[0] ){
+						++this->AnodeHighHits[0];
+						this->CurrEvt.hg.xa += evt->GetEnergy();
+						++this->CurrEvt.hg.numanodes;
+					}else{
+						++this->AnodeHighHits[0];
+					}
+				}
+			}else if( group.compare("xb") == 0 ){
+				if( islowgain ){
+					if( not this->AnodeLowHits[1] ){
+						++this->AnodeLowHits[1];
+						this->CurrEvt.lg.xb += evt->GetEnergy();
+						++this->CurrEvt.lg.numanodes;
+					}else{
+						++this->AnodeLowHits[1];
+					}
+				}else{
+					if( not this->AnodeHighHits[1] ){
+						++this->AnodeHighHits[1];
+						this->CurrEvt.hg.xb += evt->GetEnergy();
+						++this->CurrEvt.hg.numanodes;
+					}else{
+						++this->AnodeHighHits[1];
+					}
+				}
+			}else if( group.compare("ya") == 0 ){
+				if( islowgain ){
+					if( not this->AnodeLowHits[2] ){
+						++this->AnodeLowHits[2];
+						this->CurrEvt.lg.ya += evt->GetEnergy();
+						++this->CurrEvt.lg.numanodes;
+					}else{
+						++this->AnodeLowHits[2];
+					}
+				}else{
+					if( not this->AnodeHighHits[2] ){
+						++this->AnodeHighHits[2];
+						this->CurrEvt.hg.ya += evt->GetEnergy();
+						++this->CurrEvt.hg.numanodes;
+					}else{
+						++this->AnodeHighHits[2];
+					}
+				}
+			}else if( group.compare("yb") == 0 ){
+				if( islowgain ){
+					if( not this->AnodeLowHits[3] ){
+						++this->AnodeLowHits[3];
+						this->CurrEvt.lg.yb += evt->GetEnergy();
+						++this->CurrEvt.lg.numanodes;
+					}else{
+						++this->AnodeLowHits[3];
+					}
+				}else{
+					if( not this->AnodeHighHits[3] ){
+						++this->AnodeHighHits[3];
+						this->CurrEvt.hg.yb += evt->GetEnergy();
+						++this->CurrEvt.hg.numanodes;
+					}else{
+						++this->AnodeHighHits[3];
+					}
+				}
+			}else{
+				throw std::runtime_error("evt in PSPMTProcessor has malformed xml, anode is missing xa,xb,ya,yb group");
+			}
+		}else if( subtype.compare("dynode") == 0 ){
+			if( group.compare("amp") == 0 ){
+				if( not AmpDynodeHits ){
+					++this->AmpDynodeHits;
+					this->CurrEvt.ampdynode += evt->GetEnergy();
+				}else{
+					++this->AmpDynodeHits;
+				}
+			}else{
+				if( islowgain ){
+					if( not this->DynodeLowHits ){
+						++this->DynodeLowHits;
+						this->CurrEvt.lg.dynode += evt->GetEnergy();
+					}else{
+						++this->DynodeLowHits;
+					}
+				}else{
+					if( not this->DynodeHighHits ){
+						++this->DynodeHighHits;
+						this->CurrEvt.lg.dynode += evt->GetEnergy();
+					}else{
+						++this->DynodeHighHits;
+					}
+				}
+			}
+		}else{
+			throw std::runtime_error("evt in PSPMTProcessor has malformed xml, is neither anode or dynode");
+		}
+	}
+
+	if( this->CurrEvt.lg.numanodes == 4 ){
+		this->CalculatePosition(this->CurrEvt.lg,0.0,0.0,0.0,false);
+		hismanager->Fill("PSPMT_1901",this->CurrEvt.lg.position.first,this->CurrEvt.lg.position.second);
+	}
+		
+	if( this->CurrEvt.hg.numanodes == 4 ){
+		this->CalculatePosition(this->CurrEvt.hg,0.0,0.0,0.0,false);
+		hismanager->Fill("PSPMT_1902",this->CurrEvt.hg.position.first,this->CurrEvt.hg.position.second);
+	}
+
+	Processor::EndProcess();
+	return true;
+}
+
+[[maybe_unused]] bool PSPMTProcessor::Process([[maybe_unused]] EventSummary& summary,[[maybe_unused]] PLOTS::PlotRegistry* hismanager,[[maybe_unused]] CUTS::CutRegistry* cutmanager){
+	return true;
+}
+
+[[maybe_unused]] bool PSPMTProcessor::PostProcess([[maybe_unused]] EventSummary& summary,[[maybe_unused]] PLOTS::PlotRegistry* hismanager,[[maybe_unused]] CUTS::CutRegistry* cutmanager){
+	return true;
+}
+
+void PSPMTProcessor::Init(const YAML::Node& config){
+	this->console->info("Init called with YAML::Node");
+}
+
+void PSPMTProcessor::Init(const Json::Value& config){
+	this->console->info("Init called with Json::Value");
+}
+
+void PSPMTProcessor::Init(const pugi::xml_node& config){
+	this->console->info("Init called with pugi::xml_node");
+}
+		
+void PSPMTProcessor::Finalize(){
+	this->console->info("{} has been finalized",this->ProcessorName);
+}
+
+void PSPMTProcessor::DeclarePlots(PLOTS::PlotRegistry* hismanager) const{
+	hismanager->RegisterPlot<TH2F>("PSPMT_1901","Low Gain Image; Position (arb.); Position (arb.)",1024,0,1,1024,0,1);
+	hismanager->RegisterPlot<TH2F>("PSPMT_1902","High Gain Image; Position (arb.); Position (arb.)",1024,0,1,1024,0,1);
+	this->console->info("Finished Declaring Plots");
+}
+
+void PSPMTProcessor::RegisterTree([[maybe_unused]] std::unordered_map<std::string,TTree*>& outputtrees){
+}
+
+void PSPMTProcessor::CleanupTree(){
+}
+
+void PSPMTProcessor::Reset(){
+	this->PrevEvt = this->CurrEvt;
+	this->CurrEvt = this->NewEvt;
+	this->DynodeHighHits = 0;
+	this->DynodeLowHits = 0;
+	this->AnodeHighHits = std::vector<int>(4,0);
+	this->AnodeLowHits = std::vector<int>(4,0);
+}
+
+void PSPMTProcessor::CalculatePosition(PSPMTProcessor::Image& img,double rotation,double xcenter,double ycenter,bool xflip){
+	double x = 0.0;
+	double y = 0.0;
+	img.anodesum = img.xa+img.ya+img.xb+img.yb;
+	
+	if( xflip ){
+		x = (img.ya+img.xb)/img.anodesum;
+		y = (img.xa+img.xb)/img.anodesum;
+	}else{
+		x = (img.yb+img.xa)/img.anodesum;
+		y = (img.xa+img.xb)/img.anodesum;
+	}
+
+	x -= xcenter;
+	y -= ycenter;
+
+	img.position = { x*std::cos(rotation) + xcenter -y*std::sin(rotation) + ycenter, x*std::sin(rotation) + xcenter + y*std::cos(rotation) + ycenter};
+}
