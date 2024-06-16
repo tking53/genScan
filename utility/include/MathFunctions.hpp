d3f1281dacc4e36083d94ec25251534a1cb491c7
truland <tnfirebird1979@gmail.com>
2024-06-16 12:15:45 +0900

    Add in math to calculate angle between two points

---
+++ utility/include/MathFunctions.hpp
@@ -0,0 +1,8 @@
+#ifndef __MATH_FUNCTIONS_HPP__
+#define __MATH_FUNCTIONS_HPP__ 
+
+namespace Math{
+	double CalcAngleBetweenTwoPoints(double,double,double,double);
+}
+
+#endif
---
+++ utility/src/MathFunctions.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+
+#include "MathFunctions.hpp"
+
+namespace Math{
+	//calculate angle between two points relative to a common center
+	double CalcAngleBetweenTwoPoints(double x1, double y1, double x2, double y2){
+		return std::acos(-1.0*(y1*y2 + x1*x2)/(std::sqrt(x1*x1+y1*y1)*std::sqrt(x2*x2+y2*y2)));
+	}
+}
