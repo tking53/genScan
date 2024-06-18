841c59ceba09bc67093d3140ef78d5d5cb8e65ea
truland <tnfirebird1979@gmail.com>
2024-06-18 16:39:46 +0900

    MOAR PLOTS

--- processors/src/HagridProcessor.cpp
+++ processors/src/HagridProcessor.cpp
@@ -65,6 +65,11 @@ HagridProcessor::HagridProcessor(const std::string& log) : Processor(log,"Hagrid
 		int idx = 0;
 		this->CurrEvt.FirstTimeStamp = *(std::min_element(this->TimeStamps.begin(),this->TimeStamps.end()));
 		this->CurrEvt.FinalTimeStamp = *(std::max_element(this->TimeStamps.begin(),this->TimeStamps.end()));
+
+		double time_s = (this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9;
+		double time_m = time_s/60.0;
+		double time_h = time_m/60.0;
+
 		double upstreamsum = 0.0;
 		double downstreamsum = 0.0;
 		for( const auto& erg : this->CurrEvt.Energy ){
@@ -73,26 +78,42 @@ HagridProcessor::HagridProcessor(const std::string& log) : Processor(log,"Hagrid
 			if( idx%2 ){
 				downstreamsum += erg;
 				hismanager->Fill("HAGRID_5025",erg);
-				hismanager->Fill("HAGRID_5025_TIME",erg,(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+				hismanager->Fill("HAGRID_5025_TIME_S",erg,time_s);
+				hismanager->Fill("HAGRID_5025_TIME_M",erg,time_m);
+				hismanager->Fill("HAGRID_5025_TIME_H",erg,time_h);
 			}else{
 				upstreamsum += erg;
 				hismanager->Fill("HAGRID_5015",erg);
-				hismanager->Fill("HAGRID_5015_TIME",erg,(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+				hismanager->Fill("HAGRID_5015_TIME_S",erg,time_s);
+				hismanager->Fill("HAGRID_5015_TIME_M",erg,time_m);
+				hismanager->Fill("HAGRID_5015_TIME_H",erg,time_h);
 			}
 			hismanager->Fill("HAGRID_5005",erg);
-			hismanager->Fill("HAGRID_5005_TIME",erg,(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+			hismanager->Fill("HAGRID_5005_TIME_S",erg,time_s);
+			hismanager->Fill("HAGRID_5005_TIME_M",erg,time_m);
+			hismanager->Fill("HAGRID_5005_TIME_H",erg,time_h);
 
 			++idx;
 		}
 
-		hismanager->Fill("HAGRID_6000",(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+		hismanager->Fill("HAGRID_6000",time_s);
+		hismanager->Fill("HAGRID_6001",time_m);
+		hismanager->Fill("HAGRID_6002",time_h);
 
 		hismanager->Fill("HAGRID_5010",upstreamsum);
-		hismanager->Fill("HAGRID_5010_TIME",upstreamsum,(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+		hismanager->Fill("HAGRID_5010_TIME_S",upstreamsum,time_s);
+		hismanager->Fill("HAGRID_5010_TIME_M",upstreamsum,time_m);
+		hismanager->Fill("HAGRID_5010_TIME_H",upstreamsum,time_h);
+
 		hismanager->Fill("HAGRID_5020",downstreamsum);
-		hismanager->Fill("HAGRID_5020_TIME",downstreamsum,(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+		hismanager->Fill("HAGRID_5020_TIME_S",downstreamsum,time_s);
+		hismanager->Fill("HAGRID_5020_TIME_M",downstreamsum,time_m);
+		hismanager->Fill("HAGRID_5020_TIME_H",downstreamsum,time_h);
+
 		hismanager->Fill("HAGRID_5000",this->CurrEvt.TotalEnergy);
-		hismanager->Fill("HAGRID_5000_TIME",this->CurrEvt.TotalEnergy,(this->CurrEvt.FirstTimeStamp-this->FirstEvtTime)*1.0e-9);
+		hismanager->Fill("HAGRID_5000_TIME_S",this->CurrEvt.TotalEnergy,time_s);
+		hismanager->Fill("HAGRID_5000_TIME_M",this->CurrEvt.TotalEnergy,time_m);
+		hismanager->Fill("HAGRID_5000_TIME_H",this->CurrEvt.TotalEnergy,time_h);
 	}
 
 	Processor::EndProcess();
@@ -138,14 +159,37 @@ void HagridProcessor::DeclarePlots(PLOTS::PlotRegistry* hismanager) const{
 	hismanager->RegisterPlot<TH1F>("HAGRID_5020","Downstream Hagrid sum; Energy (keV)",16384,0,16384);
 	hismanager->RegisterPlot<TH1F>("HAGRID_5025","Downstream Hagrid stack; Energy (keV)",16384,0,16384);
 	
-	hismanager->RegisterPlot<TH2F>("HAGRID_5000_TIME","Hagrid sum decay; Energy (keV)",16384,0,16384,1024,0,1024);
-	hismanager->RegisterPlot<TH2F>("HAGRID_5005_TIME","Hagrid stack decay; Energy (keV)",16384,0,16384,1024,0,1024);
-	hismanager->RegisterPlot<TH2F>("HAGRID_5010_TIME","Upstream Hagrid sum decay; Energy (keV)",16384,0,16384,1024,0,1024);
-	hismanager->RegisterPlot<TH2F>("HAGRID_5015_TIME","Upstream Hagrid stack decay; Energy (keV)",16384,0,16384,1024,0,1024);
-	hismanager->RegisterPlot<TH2F>("HAGRID_5020_TIME","Downstream Hagrid sum decay; Energy (keV)",16384,0,16384,1024,0,1024);
-	hismanager->RegisterPlot<TH2F>("HAGRID_5025_TIME","Downstream Hagrid stack decay; Energy (keV)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5000_TIME_S","Hagrid sum decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5005_TIME_S","Hagrid stack decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5010_TIME_S","Upstream Hagrid sum decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5015_TIME_S","Upstream Hagrid stack decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5020_TIME_S","Downstream Hagrid sum decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5025_TIME_S","Downstream Hagrid stack decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
 	
-	hismanager->RegisterPlot<TH1F>("HAGRID_6000","Hagrid decay; Energy (keV)",65536,0,65536);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5000_TIME_M","Hagrid sum decay; Energy (keV); Time Since First Evt (min)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5005_TIME_M","Hagrid stack decay; Energy (keV); Time Since First Evt (min)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5010_TIME_M","Upstream Hagrid sum decay; Energy (keV); Time Since First Evt (min)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5015_TIME_M","Upstream Hagrid stack decay; Energy (keV); Time Since First Evt (min)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5020_TIME_M","Downstream Hagrid sum decay; Energy (keV); Time Since First Evt (min)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5025_TIME_M","Downstream Hagrid stack decay; Energy (keV); Time Since First Evt (min)",16384,0,16384,1024,0,1024);
+	
+	hismanager->RegisterPlot<TH2F>("HAGRID_5000_TIME_H","Hagrid sum decay; Energy (keV); Time Since First Evt (hr)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5005_TIME_H","Hagrid stack decay; Energy (keV); Time Since First Evt (hr)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5010_TIME_H","Upstream Hagrid sum decay; Energy (keV); Time Since First Evt (hr)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5015_TIME_H","Upstream Hagrid stack decay; Energy (keV); Time Since First Evt (hr)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5020_TIME_H","Downstream Hagrid sum decay; Energy (keV); Time Since First Evt (hr)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5025_TIME_H","Downstream Hagrid stack decay; Energy (keV); Time Since First Evt (hr)",16384,0,16384,1024,0,1024);
+	
+	hismanager->RegisterPlot<TH2F>("HAGRID_5000_TIME_S","Hagrid sum decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5005_TIME_S","Hagrid stack decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5010_TIME_S","Upstream Hagrid sum decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5015_TIME_S","Upstream Hagrid stack decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5020_TIME_S","Downstream Hagrid sum decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	hismanager->RegisterPlot<TH2F>("HAGRID_5025_TIME_S","Downstream Hagrid stack decay; Energy (keV); Time Since First Evt (s)",16384,0,16384,1024,0,1024);
+	
+	hismanager->RegisterPlot<TH1F>("HAGRID_6000","Hagrid decay; Time Since First Evt (s)",65536,0,65536);
+	hismanager->RegisterPlot<TH1F>("HAGRID_6001","Hagrid decay; Time Since First Evt (min)",65536,0,65536);
+	hismanager->RegisterPlot<TH1F>("HAGRID_6002","Hagrid decay; Time Since First Evt (hr)",65536,0,65536);
 
 	this->console->info("Finished Declaring Plots");
 }
