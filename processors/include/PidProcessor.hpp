747df6352310c35d3ba1bb0be624acd414b59fb8
Toby King <tking.91@gmail.com>
2025-03-12 16:39:07 -0400

    PidProcessor:: Trees are working. Hists are next

--- processors/include/PidProcessor.hpp
+++ processors/include/PidProcessor.hpp
@@ -2,6 +2,7 @@
 #define __PID_PROCESSOR_HPP__
 
 #include "Processor.hpp"
+#include "PidStruct.hpp"
 
 class PidProcessor : public Processor{
 	public:
@@ -21,7 +22,46 @@ class PidProcessor : public Processor{
 		virtual void RegisterTree([[maybe_unused]] std::unordered_map<std::string,TTree*>&) final;
 		virtual void CleanupTree() final;
 	private:
+
+	ProcessorStruct::DBOX db3;
+	ProcessorStruct::DBOX db4;
+	ProcessorStruct::DBOX db5;	
+	ProcessorStruct::FP fp1;
+	ProcessorStruct::FP fp2;
+	double currRF;
+	
+
+	enum DETPOSITION{
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT,
+		ANODE,
 		
+	};
+
+	enum BOXID{
+		DB3,
+		DB4,
+		DB5,
+		FP1,
+		FP2,
+		
+	};
+
+	enum DETSUBTYPE	{
+		SCINT,
+		PPAC0,
+		PPAC1,
+		PIN,
+		RF
+	};
+
+	DETPOSITION currDETPOSITION;
+	DETSUBTYPE currDETSUBTYPE;
+	BOXID currBOXID;
+
+	int numPins;
 };
 
 #endif
---
+++ processors/include/PidStructLinkDef.hpp
@@ -0,0 +1,13 @@
+#ifndef __PID_STRUCT_LINKDEF_HPP__
+#define __PID_STRUCT_LINKDEF_HPP__
+#ifdef __CINT__
+
+#pragma link C++ struct ProcessorStruct::DBOX+;
+#pragma link C++ struct ProcessorStruct::FP+;
+
+#pragma link C++ struct ProcessorStruct::SCINT+;
+#pragma link C++ struct ProcessorStruct::PPAC+;
+#pragma link C++ struct ProcessorStruct::PidDet+;
+
+#endif
+#endif //PAASS_LINKDEF_HPP
