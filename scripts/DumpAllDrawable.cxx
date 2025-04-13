5d69a8acd72a370f6210954f70d7f62d0c105a6a
truland <tnfirebird1979@gmail.com>
2025-04-13 15:39:29 -0400

    added in regex and correct filtering for TH2/TH1

--- scripts/DumpAllDrawable.cxx
+++ scripts/DumpAllDrawable.cxx
@@ -1,4 +1,5 @@
 #include <string>
+#include <regex>
 
 #include "TROOT.h"
 #include "TFile.h"
@@ -6,22 +7,43 @@
 #include "TKey.h"
 #include "TCanvas.h"
 #include "TH1.h"
+#include "TH2.h"
 
-void DumpAllDrawable(const std::string& filename="GenPeakFitterResults.root",const std::string& drawablename="TH1",const std::string& drawoptions=""){
+void DumpTH1(TFile*& f,const std::string& actualname,const std::string& drawoptions,double xlow,double xhigh){
+	auto currobj = dynamic_cast<TH1*>(f->Get(actualname.c_str()));
+	currobj->GetXaxis()->SetRangeUser(xlow,xhigh);
+	currobj->Draw(drawoptions.c_str());
+}
+
+void DumpTH2(TFile*& f,const std::string& actualname,const std::string& drawoptions,double xlow,double xhigh,double ylow,double yhigh){
+	auto currobj = dynamic_cast<TH2*>(f->Get(actualname.c_str()));
+	currobj->GetXaxis()->SetRangeUser(xlow,xhigh);
+	currobj->GetYaxis()->SetRangeUser(ylow,yhigh);
+	currobj->Draw(drawoptions.c_str());
+}
+
+void DumpAllDrawable(const std::string& filename="GenPeakFitterResults.root",const std::string& restr = ".*",const std::string& drawablename="TH1",const std::string& drawoptions="",double xlow = 1.0,double xhigh = 4000.0,double ylow = 1.0, double yhigh = 4000.0){
 	TFile* f = new TFile(filename.c_str(),"OPEN");
 	TIter next(f->GetListOfKeys());
 	TKey *key;
+	std::regex re(restr);
 
 	while ((key = (TKey*)next())) {
 		TClass *clsPtr = gROOT->GetClass(key->GetClassName());
-		TString name = key->GetClassName();
-		if( name.Contains(drawablename.c_str()) ){
-			auto canvasname = std::string(key->GetName())+"_Scratch";
-			auto currcanvas = new TCanvas(canvasname.c_str(),canvasname.c_str(),600,600);
-			auto currobj = dynamic_cast<TH1*>(f->Get(key->GetName()));
-			currobj->GetXaxis()->SetRangeUser(1.0,4000.0);
-			currobj->Draw(drawoptions.c_str());
-			currcanvas->SetLogy(1);
+		auto actualname = std::string(key->GetName());
+		if( std::regex_match(actualname,re) ){
+			TString name = key->GetClassName();
+			if( name.Contains(drawablename.c_str()) ){
+				auto canvasname = actualname+"_Scratch";
+				auto currcanvas = new TCanvas(canvasname.c_str(),canvasname.c_str(),600,600);
+				if( name.Contains("TH1") ){
+					DumpTH1(f,actualname,drawoptions,xlow,xhigh);
+					currcanvas->SetLogy(1);
+				}
+				if( name.Contains("TH2") ){
+					DumpTH2(f,actualname,drawoptions,xlow,xhigh,ylow,yhigh);
+				}
+			}
 		}
 	}
 }
