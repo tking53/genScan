719727ea73b241726020b7d235e746d8df5bbd71
truland <tnfirebird1979@gmail.com>
2025-10-09 00:13:14 -0400

    add in more useful plots

--- merger/mergermain.cpp
+++ merger/mergermain.cpp
@@ -143,8 +143,7 @@ int main(int argc, char *argv[]) {
 		std::map<std::string,PLOTS::HisHelper1D> His1D = {
 			{"TDiff_Beta_Ion_s",{1000,backward_corr_time,forward_corr_time}},
 			{"TDiff_Beta_Ion_ms",{10000,backward_corr_time*1000,forward_corr_time*1000}},
-			{"TDiff_Beta_Ion_us",{10000,backward_corr_time*1000,forward_corr_time*1000}},
-			{"Radius_Beta_Ion",{1000,0,10}}
+			{"TDiff_Beta_Ion_us",{10000,backward_corr_time*1000,forward_corr_time*1000}}
 		};
 
 		for( const auto& kv : doc["HISTOGRAM1D"] ){
@@ -156,13 +155,20 @@ int main(int argc, char *argv[]) {
 		}
 
 		std::map<std::string,PLOTS::HisHelper2D> His2D = {
-			{"Implant_Radius_Beta_Ion",{16384,0,16384,1000,0,10}},
-			{"Sparse_Implant_Radius_Beta_Ion",{16384,0,65536,1000,0,10}},
-			{"AnodeSum_Radius_Beta_Ion",{16384,0,16384,1000,0,10}},
-			{"Sparse_AnodeSum_Radius_Beta_Ion",{16384,0,65536,1000,0,10}},
-			{"TDiff_Radius_Beta_Ion_s",{1000,backward_corr_time,forward_corr_time,1000,0,10}},
-			{"Mtas_TDiff_Beta_Ion_Gamma_s",{16384,0,16384,1000,backward_corr_time,forward_corr_time}},
-			{"Sparse_Mtas_TDiff_Beta_Ion_Gamma_s",{16384,0,65536,1000,backward_corr_time,forward_corr_time}}
+			{"Mtas_T_TDiff_Beta_Ion_Gamma_s",{16384,0,16384,1000,backward_corr_time,forward_corr_time}},
+			{"Sparse_Mtas_T_TDiff_Beta_Ion_Gamma_s",{16384,0,65536,1000,backward_corr_time,forward_corr_time}},
+
+			{"Mtas_TDiff_C_Beta_Ion_Gamma_s",{16384,0,16384,1000,backward_corr_time,forward_corr_time}},
+			{"Sparse_Mtas_C_TDiff_Beta_Ion_Gamma_s",{16384,0,65536,1000,backward_corr_time,forward_corr_time}},
+
+			{"Mtas_I_TDiff_Beta_Ion_Gamma_s",{16384,0,16384,1000,backward_corr_time,forward_corr_time}},
+			{"Sparse_Mtas_I_TDiff_Beta_Ion_Gamma_s",{16384,0,65536,1000,backward_corr_time,forward_corr_time}},
+
+			{"Mtas_M_TDiff_Beta_Ion_Gamma_s",{16384,0,16384,1000,backward_corr_time,forward_corr_time}},
+			{"Sparse_Mtas_M_TDiff_Beta_Ion_Gamma_s",{16384,0,65536,1000,backward_corr_time,forward_corr_time}},
+
+			{"Mtas_O_TDiff_Beta_Ion_Gamma_s",{16384,0,16384,1000,backward_corr_time,forward_corr_time}},
+			{"Sparse_Mtas_O_TDiff_Beta_Ion_Gamma_s",{16384,0,65536,1000,backward_corr_time,forward_corr_time}}
 		};
 
 		for( const auto& kv : doc["HISTOGRAM2D"] ){
@@ -207,15 +213,16 @@ int main(int argc, char *argv[]) {
 		HistogramManager->RegisterPlot<TH1F>("TDiff_Beta_Ion_ms","TDiff [Beta - Ion]; Time Difference (ms); Counts per ms",His1D["TDiff_Beta_Ion_ms"]);
 		HistogramManager->RegisterPlot<TH1F>("TDiff_Beta_Ion_us","TDiff [Beta - Ion]; Time Difference (ms); Counts per ms",His1D["TDiff_Beta_Ion_us"]);
 		
-		HistogramManager->RegisterPlot<TH1F>("Radius_Beta_Ion","Radius [Beta - Ion]; Radius (arb.); ",His1D["Radius_Beta_Ion"]);
-
-		HistogramManager->RegisterPlot<TH2F>("Implant_Radius_Beta_Ion","Radius vs Energy [Beta - Ion]; Energy (keV); Radius (arb.); ",His2D["Implant_Radius_Beta_Ion"]);
-		HistogramManager->RegisterPlot<TH2F>("Sparse_Implant_Radius_Beta_Ion","Radius vs Energy [Beta - Ion]; Energy (keV); Radius (arb.); ",His2D["Sparse_Implant_Radius_Beta_Ion"]);
-		HistogramManager->RegisterPlot<TH2F>("TDiff_Radius_Beta_Ion_s","Radius vs TDiff [Beta - Ion];  TDiff (s); Radius (arb.)",His2D["TDiff_Radius_Beta_Ion_s"]);
-		HistogramManager->RegisterPlot<TH2F>("AnodeSum_Radius_Beta_Ion","Radius vs Energy [Beta - Ion]; Energy (keV); Radius (arb.); ",His2D["AnodeSum_Radius_Beta_Ion"]);
-		HistogramManager->RegisterPlot<TH2F>("Sparse_AnodeSum_Radius_Beta_Ion","Radius vs Energy [Beta - Ion]; Energy (keV); Radius (arb.); ",His2D["Sparse_AnodeSum_Radius_Beta_Ion"]);
-		HistogramManager->RegisterPlot<TH2F>("Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Mtas_TDiff_Beta_Ion_Gamma_s"]);
-		HistogramManager->RegisterPlot<TH2F>("Sparse_Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Sparse_Mtas_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Mtas_T_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Mtas_T_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Sparse_T_Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Sparse_T_Mtas_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Mtas_C_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Mtas_C_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Sparse_C_Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Sparse_C_Mtas_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Mtas_I_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Mtas_I_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Sparse_I_Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Sparse_I_Mtas_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Mtas_M_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Mtas_M_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Sparse_M_Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Sparse_M_Mtas_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Mtas_O_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Mtas_O_TDiff_Beta_Ion_Gamma_s"]);
+		HistogramManager->RegisterPlot<TH2F>("Sparse_O_Mtas_TDiff_Beta_Ion_Gamma_s","TDiff vs Energy [Beta - Ion - Gamma]; Energy (keV); TDiff (s); ",His2D["Sparse_O_Mtas_TDiff_Beta_Ion_Gamma_s"]);
 		
 		console->info("Generating {}.list file that contains all the declared histograms",StringManip::GetFileBaseName(outputprefix));
 		HistogramManager->WriteInfo();
@@ -417,17 +424,26 @@ int main(int argc, char *argv[]) {
 					HistogramManager->Fill("TDiff_Beta_Ion_s",tdiff);
 					HistogramManager->Fill("TDiff_Beta_Ion_ms",1.0e3*tdiff);
 					HistogramManager->Fill("TDiff_Beta_Ion_us",1.0e6*tdiff);
-					HistogramManager->Fill("Radius_Beta_Ion",radius);
-
-					HistogramManager->Fill("Implant_Radius_Beta_Ion",beta_erg,radius);
-					HistogramManager->Fill("Sparse_Implant_Radius_Beta_Ion",beta_erg,radius);
-					HistogramManager->Fill("AnodeSum_Radius_Beta_Ion",beta_anode_sum,radius);
-					HistogramManager->Fill("Sparse_AnodeSum_Radius_Beta_Ion",beta_anode_sum,radius);
-					HistogramManager->Fill("TDiff_Radius_Beta_Ion_s",tdiff,radius);
 
 					const auto T = ValidTotals[iter][0].sumenergy;
-					HistogramManager->Fill("Mtas_TDiff_Beta_Ion_Gamma_s",T,tdiff);
-					HistogramManager->Fill("Sparse_Mtas_TDiff_Beta_Ion_Gamma_s",T,tdiff);
+					HistogramManager->Fill("Mtas_T_TDiff_Beta_Ion_Gamma_s",T,tdiff);
+					HistogramManager->Fill("Sparse_T_Mtas_TDiff_Beta_Ion_Gamma_s",T,tdiff);
+
+					const auto C = ValidTotals[iter][1].sumenergy;
+					HistogramManager->Fill("Mtas_C_TDiff_Beta_Ion_Gamma_s",C,tdiff);
+					HistogramManager->Fill("Sparse_C_Mtas_TDiff_Beta_Ion_Gamma_s",C,tdiff);
+
+					const auto I = ValidTotals[iter][2].sumenergy;
+					HistogramManager->Fill("Mtas_I_TDiff_Beta_Ion_Gamma_s",I,tdiff);
+					HistogramManager->Fill("Sparse_I_Mtas_TDiff_Beta_Ion_Gamma_s",I,tdiff);
+
+					const auto M = ValidTotals[iter][3].sumenergy;
+					HistogramManager->Fill("Mtas_M_TDiff_Beta_Ion_Gamma_s",M,tdiff);
+					HistogramManager->Fill("Sparse_M_Mtas_TDiff_Beta_Ion_Gamma_s",M,tdiff);
+
+					const auto O = ValidTotals[iter][4].sumenergy;
+					HistogramManager->Fill("Mtas_O_TDiff_Beta_Ion_Gamma_s",O,tdiff);
+					HistogramManager->Fill("Sparse_O_Mtas_TDiff_Beta_Ion_Gamma_s",O,tdiff);
 				}
 			}
 			++iiter;
